cabcd800151ef59060da49e13a8931a3f671d60f
Ilya <ilya.d.moroz@gmail.com>
2020-09-26 10:29:47 +0300

    update styles

--- src/App.js
+++ src/App.js
@@ -32,39 +32,30 @@ function App() {
     const month = months[d.getMonth()];
     const year = d.getFullYear();
 
-    return `${day} ${date} ${month} ${year}`
+    return `${day} ${date} ${month}, ${year}`
   }
 
   return (
-    <div className="App">
-      heLLO
-      <main>
-        <div className="search-box">
-          <input 
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={e => setQuery(e.target.value)}
-            value={query}
-            onKeyPress={search}
-          />
-        </div>
-        {(typeof weather.main != "undefined") ? (
-        <div>
-          <div className="location-box">
-            <div className="location">{weather.name}, {weather.sys.country}</div>
-            <div className="date">{dateBuilder(new Date())}</div>
-          </div>
-          <div className="weather-box">
-            <div className="temp">
-              {Math.round(weather.main.temp)}°c
+    <div className="main-container">
+    <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+    {weather.main && (
+        <div className="city">
+            <h2 className="city-name">
+                <span>{weather.name}</span>
+                <sup>{weather.sys.country}</sup>
+            </h2>
+                <div className="date">{dateBuilder(new Date())}</div>
+            <div className="city-temp">
+                {Math.round(weather.main.temp)}
+                <sup>&deg;C</sup>
             </div>
-            <div className="weather">{weather.weather[0].main}</div>
-          </div>
+            <div className="info">
+                <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                <p>{weather.weather[0].description}</p>
+            </div>
         </div>
-        ) : ('')}
-      </main>
-    </div>
+    )}
+</div>
   );
 }
 
