34e0103bfd29ee211aa3f93f0d6e0b4872d79b65
Ilya <ilya.d.moroz@gmail.com>
2020-09-26 09:50:19 +0300

    add api key

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+
+const api = {
+  key: "3163660e55591e48bebbd91b01176891",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 function App() {
   return (
