97b6ebeabff44eeb3cc8dc30a676cb34fc559fe0
Ilya <ilya.d.moroz@gmail.com>
2020-09-26 09:54:08 +0300

    add sample API call

--- src/App.js
+++ src/App.js
@@ -7,9 +7,35 @@ const api = {
 }
 
 function App() {
+
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
   return (
     <div className="App">
       heLLO
+      <div className="search-box">
+          <input 
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          />
+        </div>
     </div>
   );
 }
