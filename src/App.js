edadce5c86c88f08e9eda1f03ce7ef53ae5c9320
Ilya <ilya.d.moroz@gmail.com>
2020-09-26 11:09:24 +0300

    remove localStorage hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import useLocalStorage from './utils/useLocalStorage.js';
 import { fetchWeather } from './api/fetchWeather';
 
 function App() {
 
-  const [query, setQuery] = useState('');
+  let [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
-  // const [city, setCity] = useLocalStorage('city', 'Berlin');
+
 
   const search = async (evt) => {
     if (evt.key === "Enter") {
@@ -32,28 +31,29 @@ function App() {
 
   return (
     <div className="main-container">
-    <input type="text"className="search"placeholder="Search..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
-    {weather.main && (
-        <div className="city">
-            <h2 className="city-name">
-                <span>{weather.name}</span>
-                <sup>{weather.sys.country}</sup>
-            </h2>
-                <div className="date">{dateBuilder(new Date())}</div>
-            <div className="city-temp">
-                {Math.round(weather.main.temp)}
-                <sup>&deg;C</sup>
-            </div>
-            <div className="info">
-                <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
-                <p>{weather.weather[0].description}</p>
-            </div>
-            <div className="info">
-                <p>Wind: {weather.wind.speed} m/s</p>
-            </div>
-        </div>
-    )}
-</div>
+      <h1>Weather app</h1>
+      <input type="text"className="search"placeholder="Search city..."value={query}onChange={(e) => setQuery(e.target.value)}onKeyPress={search}/>
+      {weather.main && (
+          <div className="city">
+              <h2 className="city-name">
+                  <span>{weather.name}</span>
+                  <sup>{weather.sys.country}</sup>
+              </h2>
+                  <div className="date">{dateBuilder(new Date())}</div>
+              <div className="city-temp">
+                  {Math.round(weather.main.temp)}
+                  <sup>&deg;C</sup>
+              </div>
+              <div className="info">
+                  <img className="city-icon" src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
+                  <p>{weather.weather[0].description}</p>
+              </div>
+              <div className="info">
+                  <p>Wind: {weather.wind.speed} m/s</p>
+              </div>
+          </div>
+      )}
+  </div>
   );
 }
 
